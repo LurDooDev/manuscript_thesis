bfe75e0f237dd8d09b1c322f6ef65b6992ae62c0
LurDooDev <111673346+LurDooDev@users.noreply.github.com>
2024-10-07 05:48:40 +0800

    static files

--- ccswebsite/ccswebsite/settings.py
+++ ccswebsite/ccswebsite/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ccswebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'ccswebsite/static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
